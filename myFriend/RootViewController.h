d3d1c5f839da0d584c322f26861b56bf92a2a6d3
Marlen Waaijer <marlenwaaijer@waterland.home>
2011-08-01 19:11:14 -0400

    Initial Commit

---
+++ myFriend/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  myFriend
+//
+//  Created by Marlen Waaijer on 8/1/11.
+//  Copyright 2011 KARENZA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ myFriend/myFriendAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  myFriendAppDelegate.h
+//  myFriend
+//
+//  Created by Marlen Waaijer on 8/1/11.
+//  Copyright 2011 KARENZA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myFriendAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
